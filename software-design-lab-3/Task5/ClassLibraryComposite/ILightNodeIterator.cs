7a0880958a150ce058d817690ffef2dd45be36a0
Didenko Daniil <ipz235_ddo@student.ztu.edu.ua>
2025-05-07 19:16:54 +0300

    #1 Added Iterator pattern to lab3 task5

---
+++ software-design-lab-3/Task5/ClassLibraryComposite/ILightNodeIterator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryComposite
+{
+    public interface ILightNodeIterator
+    {
+        IEnumerable<LightNode> DepthFirst();
+        IEnumerable<LightNode> BreadthFirst();
+    }
+}
