9bb2a8424a12d0df282d83e271aa04cb7d1d73ce
Didenko Daniil <ipz235_ddo@student.ztu.edu.ua>
2025-05-08 19:54:30 +0300

    #5 Added state pattern lab 3, task 5

---
+++ software-design-lab-3/Task5/ClassLibraryComposite/States/ActiveState.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibraryComposite.interfaces;
+using System;
+
+namespace ClassLibraryComposite.States
+{
+    public class ActiveState : ILightNodeState
+    {
+        public void OnEnter(LightElementNode node)
+        {
+            node.RemoveClass("hover");
+            node.AddClass("active");
+            Console.WriteLine("Entered Active state.");
+        }
+
+        public void OnExit(LightElementNode node)
+        {
+            node.RemoveClass("active");
+            Console.WriteLine("Exited Active state.");
+        }
+    }
+
+}
---
+++ software-design-lab-3/Task5/ClassLibraryComposite/States/NormalState.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibraryComposite.interfaces;
+using System;
+
+namespace ClassLibraryComposite.States
+{
+    public class NormalState : ILightNodeState
+    {
+        public void OnEnter(LightElementNode node)
+        {
+            node.RemoveClass("hover");
+            node.RemoveClass("active");
+            node.AddClass("normal");
+            Console.WriteLine("Entered Normal state.");
+        }
+
+        public void OnExit(LightElementNode node)
+        {
+            node.RemoveClass("normal");
+            Console.WriteLine("Exited Normal state.");
+        }
+    }
+
+}
